4e2f5682d30a8d57f36f0e44bf46c751d2d81e57
Pedro Emanuel <emanuelpedro381@gmail.com>
2022-03-22 15:17:10 -0300

    client files

---
+++ components/message.jsx
@@ -0,0 +1,12 @@
+import styles from "../styles/Message.module.css"
+
+function Message(props){
+    return(
+        props.author === props.message.author ?
+        <div className={styles.message_from_me}>você: {props.message.message}<br/></div>
+        :
+        <div className={styles.message}>{props.message.user}: {props.message.message}<br/></div>
+    )
+}
+
+export default Message
