5d2d3b1bef4f607b4a72063a80c254a08355715b
Pedro Emanuel <emanuelpedro381@gmail.com>
2022-03-23 16:53:00 -0300

    increased performance

--- components/chat.jsx
+++ components/chat.jsx
@@ -2,15 +2,12 @@ import { useContext, useEffect, useState } from "react"
 import { SocketContext, useMessages } from "../contexts/contextsocket"
 import Message from "./message"
 
-const server_messages = []
-
 function Chat(){
     const { socket } = useContext(SocketContext)
     const [mensagens, setMessages] = useState([])
 
     socket.on("updateMessages",(data)=>{
         setMessages(data)
-        socket.emit('listened')
     })
 
     return(
